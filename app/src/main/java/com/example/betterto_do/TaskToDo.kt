405e4aa8ff3733bf599cc2223f80b2e3c7c8bc84
Jaiten Beck <jaitenbeck@yahoo.com>
2023-11-01 23:32:40 -0600

    Added variable in TaskToDo
    Added Boolean variable to keep track of task completion

--- app/src/main/java/com/example/betterto_do/TaskToDo.kt
+++ app/src/main/java/com/example/betterto_do/TaskToDo.kt
@@ -1,13 +1,15 @@
 package com.example.betterto_do
 
+//This is imported to use the LocalTime for duedate
 import java.time.LocalTime
 
 //Class to handle tasks
 class TaskToDo {
-    //variable for task title/name
     var taskName: String = ""
-    //variable for description that can be removed if we decide it's not needed
+    //variable for description (may or may not keep this)
     var taskInfo: String = ""
     //This should be able to set due date to the time on the user device
     var dueDate: LocalTime? = null
+    //To track task completion and will start as "not completed"
+    var taskCompleted: Boolean = false
 }

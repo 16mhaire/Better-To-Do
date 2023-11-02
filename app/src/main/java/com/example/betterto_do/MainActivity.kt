c28016e212b206f1ea7651181c413784f3f4c128
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-02 15:58:52 -0400

    navigation not building
    # Conflicts:
    #	.idea/vcs.xml
    #	app/build/intermediates/apk_ide_redirect_file/debug/redirect.txt
    #	app/build/intermediates/dex/debug/mergeProjectDexDebug/13/classes.dex
    #	app/build/intermediates/dex/debug/mergeProjectDexDebug/8/classes.dex
    #	app/build/intermediates/dex_archive_input_jar_hashes/debug/out
    #	app/build/intermediates/incremental/debug-mergeJavaRes/merge-state
    #	app/build/intermediates/incremental/debug/mergeDebugResources/compile-file-map.properties
    #	app/build/intermediates/incremental/debug/mergeDebugResources/merger.xml
    #	app/build/intermediates/incremental/debug/packageDebugResources/compile-file-map.properties
    #	app/build/intermediates/incremental/debug/packageDebugResources/merger.xml
    #	app/build/intermediates/incremental/packageDebug/tmp/debug/dex-renamer-state.txt
    #	app/build/intermediates/incremental/packageDebug/tmp/debug/zip-cache/androidResources
    #	app/build/intermediates/manifest_merge_blame_file/debug/manifest-merger-blame-debug-report.txt
    #	app/build/intermediates/merged_res_blame_folder/debug/out/multi-v2/debug.json
    #	app/build/intermediates/merged_res_blame_folder/debug/out/multi-v2/values.json
    #	app/build/intermediates/processed_res/debug/out/resources-debug.ap_
    #	app/build/intermediates/project_dex_archive/debug/out/com/example/betterto_do/LiveLiterals$DashboardKt.dex
    #	app/build/intermediates/project_dex_archive/debug/out/com/example/betterto_do/LiveLiterals$MainActivityKt.dex
    #	app/build/intermediates/project_dex_archive/debug/out/com/example/betterto_do/ui/theme/LiveLiterals$ThemeKt.dex
    #	app/build/intermediates/source_set_path_map/debug/file-map.txt
    #	app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/inputs/source-to-output.tab
    #	app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/inputs/source-to-output.tab.keystream
    #	app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/inputs/source-to-output.tab.values.at
    #	app/build/kotlin/compileDebugKotlin/cacheable/caches-jvm/inputs/source-to-output.tab_i
    #	app/build/kotlin/compileDebugKotlin/cacheable/last-build.bin
    #	app/build/kotlin/compileDebugKotlin/local-state/build-history.bin
    #	app/build/tmp/kotlin-classes/debug/com/example/betterto_do/LiveLiterals$DashboardKt.class
    #	app/build/tmp/kotlin-classes/debug/com/example/betterto_do/LiveLiterals$MainActivityKt.class
    #	app/build/tmp/kotlin-classes/debug/com/example/betterto_do/ui/theme/LiveLiterals$ThemeKt.class

--- app/src/main/java/com/example/betterto_do/MainActivity.kt
+++ app/src/main/java/com/example/betterto_do/MainActivity.kt
@@ -6,45 +6,62 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.betterto_do.ui.theme.BetterToDoTheme
-import com.example.betterto_do.Dashboard
-
+import androidx.compose.material3.Text
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BetterToDoTheme {
-                // A surface container using the 'background' color from the theme
+            val navController = rememberNavController()
+
+            // Use the theme from MaterialTheme
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Dashboard() // Use the Dashboard Composable here
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ) {
+                        // Add the HomeScreen composable for the "home" destination
+                        composable("home") {
+                            HomeScreen(navController)
+                        }
+
+                        // Add more destinations here
+                        composable("new_destination/{itemID}") { backStackEntry ->
+                            val itemID = backStackEntry.arguments?.getString("itemID")
+                            // Use the itemID to display the details of the selected item
+                            // You can pass this itemID to the composable for the new destination
+                            // Create a composable for this destination and use the itemID
+                        }
+
+                        // Define routes for other screens as needed
+                    }
                 }
             }
         }
+    }
 
+    @Preview(showBackground = true)
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-        @Composable
-        fun Greeting(name: String, modifier: Modifier = Modifier) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-
-        @Preview(showBackground = true)
-        @Composable
-        fun GreetingPreview() {
-            BetterToDoTheme {
-                Greeting("Android")
-            }
-        }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Greeting("Android")
     }
 }
-/// anita test

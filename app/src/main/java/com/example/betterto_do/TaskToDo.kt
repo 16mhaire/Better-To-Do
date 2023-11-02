1282e63979b8bc9c74ce40316fe9e84287bb9ab0
Jaiten Beck <jaitenbeck@yahoo.com>
2023-11-01 23:22:17 -0600

    Added comments to TaskToDo
    Added comments to explain what is going on in TaskToDo class

--- app/src/main/java/com/example/betterto_do/TaskToDo.kt
+++ app/src/main/java/com/example/betterto_do/TaskToDo.kt
@@ -2,8 +2,12 @@ package com.example.betterto_do
 
 import java.time.LocalTime
 
+//Class to handle tasks
 class TaskToDo {
+    //variable for task title/name
     var taskName: String = ""
+    //variable for description that can be removed if we decide it's not needed
     var taskInfo: String = ""
+    //This should be able to set due date to the time on the user device
     var dueDate: LocalTime? = null
 }

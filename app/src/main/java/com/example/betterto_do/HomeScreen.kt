05c480801b908e83f52e2a90f73d276c17e2dd2c
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-02 10:15:32 -0400

    navigation builds

--- app/src/main/java/com/example/betterto_do/HomeScreen.kt
+++ app/src/main/java/com/example/betterto_do/HomeScreen.kt
@@ -1,10 +1,25 @@
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+package com.example.betterto_do
+
+import com.example.betterto_do.navigateToHome
+import com.example.betterto_do.navigateToNewDestination
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -20,18 +35,14 @@ fun HomeScreen(navController: NavController) {
     ) {
         Text(
             text = "Welcome to the Home Screen",
-            style = MaterialTheme.typography.subtitle1, // Use a different typography style
+            style = MaterialTheme.typography.headlineLarge, // Use a different typography style, e.g., h4
             fontWeight = FontWeight.Bold, // Add fontWeight
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Enter your name") },
-            modifier = Modifier.fillMaxWidth()
-        )
+
+    
 
         Spacer(modifier = Modifier.height(16.dp))
 

130ed11237d87d80a2a2d5200c57e4bc4b4c31e4
Jaiten Beck <jaitenbeck@yahoo.com>
2023-11-01 23:37:29 -0600

    TaskToDo
    Added variable for task ID so that we can find tasks within future lists

--- app/src/main/java/com/example/betterto_do/TaskToDo.kt
+++ app/src/main/java/com/example/betterto_do/TaskToDo.kt
@@ -2,6 +2,8 @@ package com.example.betterto_do
 
 //This is imported to use the LocalTime for duedate
 import java.time.LocalTime
+//Imported for task id's 
+import java.util.UUID
 
 //Class to handle tasks
 class TaskToDo {
@@ -10,6 +12,8 @@ class TaskToDo {
     var taskInfo: String = ""
     //This should be able to set due date to the time on the user device
     var dueDate: LocalTime? = null
-    //To track task completion and will start as "not completed"
+    //To track task completion and will start as "not completed" or false
     var taskCompleted: Boolean = false
+    //Adding an id to track the task when it is in a list
+    var taskID: UUID = UUID.randomUUID()
 }

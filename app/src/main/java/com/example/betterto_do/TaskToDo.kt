1daa9cf991fb465c99979c4e5033becb5f1831fe
Jaiten Beck <jaitenbeck@yahoo.com>
2023-11-01 23:18:12 -0600

    Added TaskToDo Class
    Added a TaskToDo Class for the tasks that has the variables that we can change as needed as we go. Also commented out the @Preview line in Main Activity due to an error it was throwing.

---
+++ app/src/main/java/com/example/betterto_do/TaskToDo.kt
@@ -0,0 +1,9 @@
+package com.example.betterto_do
+
+import java.time.LocalTime
+
+class TaskToDo {
+    var taskName: String = ""
+    var taskInfo: String = ""
+    var dueDate: LocalTime? = null
+}

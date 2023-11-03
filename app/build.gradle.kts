f04a1b8d7f540eefc499bbab0c4c0ddcd94ea5f4
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-03 13:13:23 -0400

    Added Implentation implementation 'com.google.firebase:firebase-analytics:20.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,5 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("com.google.firebase:firebase-analytics:20.0.1")
 }

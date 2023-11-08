20571a2ae64aa47e1c9352d8be8ef5d35419b5db
kevin stoelting <kMstoelting@student.fullsail.edu>
2023-11-07 20:20:07 -0800

    Adding my List Feature, also I dont know why theres so many files to upload

--- app/src/main/java/com/example/betterto_do/ListScreen.kt
+++ app/src/main/java/com/example/betterto_do/ListScreen.kt
@@ -1,22 +1,123 @@
 package com.example.betterto_do
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.inappmessaging.model.Button
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@Preview
 fun ListCreationScreen(){
 
     var listTitle by remember { mutableStateOf("")}
+    var isImportant by remember { mutableStateOf(false)}
+    var isDropdownExpanded by remember { mutableStateOf(false)}
+    var labels = listOf("Label 1", "Label 2", "Label 3")
+    var selectedLabel by remember { mutableStateOf("")}
 
-    TextField(
-        value = listTitle,
-        onValueChange = {newValue -> listTitle = newValue} )
 
-}
+Card(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        TextField(
+            value = listTitle,
+            onValueChange = { newValue -> listTitle = newValue },
+            label = { Text("List Title") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Select a Label:")
+        OutlinedTextField(
+            value = selectedLabel,
+            onValueChange = { newValue -> selectedLabel = newValue },
+            singleLine = true,
+            readOnly = true, //Change when we want to make lables editable
+            trailingIcon = {
+                IconButton(onClick = { isDropdownExpanded}) {
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        /*DropdownMenu(
+            expanded = isDropdownExpanded,
+            onDismissRequest = { isDropdownExpanded = false }
+        ) {
+            labels.forEach { label ->
+                if (label.isNotBlank()) {
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedLabel = label
+                            isDropdownExpanded = false
+                        }
+                    ) {
+                        Text(text = label)
+                    }
+                }
+            }
+        }*/
+
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Importance:")
+            Switch(
+                checked = isImportant,
+                onCheckedChange = { newValue -> isImportant = newValue }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
+}}
+
+
+
+
+    /*Button(
+        onClick = { /* Save this List */ },
+        modifier = Modifier.padding(16.dp)
+    ) { Text("Save") }*/
+

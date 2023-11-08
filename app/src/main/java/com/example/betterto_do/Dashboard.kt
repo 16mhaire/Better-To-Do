6b46147a56696fe77dbd92f08c109c2ab2650355
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-08 14:14:46 -0500

    adding fun stuff on the dashboard

--- app/src/main/java/com/example/betterto_do/Dashboard.kt
+++ app/src/main/java/com/example/betterto_do/Dashboard.kt
@@ -2,6 +2,7 @@
 
 package com.example.betterto_do
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,80 +19,98 @@ data class Task(val name: String)
 
 @Composable
 fun Dashboard() {
-    val tasks = remember { mutableStateOf(listOf(Task("Task 1"), Task("Task 2"), Task("Task 3"))) }
+    val tasks = remember { mutableStateOf(listOf(Task("TODAY"), Task("ALL"), Task("Scheduled"), Task("Important"))) }
     var currentScreen: String by remember { mutableStateOf("Dashboard") }
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Welcome to the Landing page (Dashboard)")
+    Box {
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Top
 
-        Button(
-            onClick = {
-                // Handle button click here
-                currentScreen = "Tasks"
-            },
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text("Go to Tasks")
-        }
-
-        LazyColumn {
-            items(tasks.value) { task ->
-                TaskWidget(task)
-            }
-        }
-
-        if (currentScreen == "Dashboard") {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Button(
-                    onClick = {
-                        // Handle button click to add a new task
-                    },
-                    modifier = Modifier.weight(1f),
-                ) {
-                    Text(text = "Add Task", color = Color.White)
-                }
+                Text(text = "Welcome to the Landing page (Dashboard)")
 
                 Button(
                     onClick = {
-                        currentScreen = "Login" // Navigate to the Login screen
+                        // Handle button click here
+                        currentScreen = "Tasks"
                     },
-                    modifier = Modifier.weight(1f),
+                    modifier = Modifier.padding(16.dp)
                 ) {
-                    Text(text = "Login", color = Color.White)
+                    Text("Go to Tasks")
                 }
 
-                Button(
-                    onClick = {
-                        currentScreen = "Register" // Navigate to the Register screen
-                    },
-                    modifier = Modifier.weight(1f),
-                ) {
-                    Text(text = "Register", color = Color.White)
+                LazyColumn {
+                    items(tasks.value) { task ->
+                        TaskWidget(task)
+                    }
+                }
+
+                if (currentScreen == "Dashboard") {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Button(
+                            onClick = {
+                                // Handle button click to add a new task
+                            },
+                            modifier = Modifier.weight(1f),
+                        ) {
+                            Text(text = "Add Task", color = Color.White)
+                        }
+
+                        Button(
+                            onClick = {
+                                currentScreen = "Login" // Navigate to the Login screen
+                            },
+                            modifier = Modifier.weight(1f),
+                        ) {
+                            Text(text = "Login", color = Color.White)
+                        }
+
+                        Button(
+                            onClick = {
+                                currentScreen = "Register" // Navigate to the Register screen
+                            },
+                            modifier = Modifier.weight(1f),
+                        ) {
+                            Text(text = "Register", color = Color.White)
+                        }
+                    }
+                } else if (currentScreen == "Login") {
+                    // Show the Login screen
+                    Login()
+                } else if (currentScreen == "Register") {
+                    // Show the Register screen
+                    Register()
                 }
             }
-        } else if (currentScreen == "Login") {
-            // Show the Login screen
-            Login()
-        } else if (currentScreen == "Register") {
-            // Show the Register screen
-            Register()
         }
     }
 }
 
 @Composable
 fun TaskWidget(task: Task) {
-    // Custom widget to display a task
-    Text(text = task.name)
-    // Add more elements as needed
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Magenta) // Set the background color of the box
+            .padding(8.dp) // Add padding around the box
+    ) {
+        Text(
+            text = task.name,
+            color = Color.Black, // Set text color
+            modifier = Modifier.padding(8.dp) // Add padding to the text
+        )
+    }
 }
+
 
 @ExperimentalMaterial3Api
 @Composable

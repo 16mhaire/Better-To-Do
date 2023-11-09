ec060fedbee34a5488f247526d177ad25fe5e324
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-08 22:44:13 -0500

    please for the love of pete commit

--- app/src/main/java/com/example/betterto_do/ui/theme/Color.kt
+++ app/src/main/java/com/example/betterto_do/ui/theme/Color.kt
@@ -1,11 +1,9 @@
 package com.example.betterto_do.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryPink = Color(0xFFB02B6A)
+val SecondaryLightPink = Color(0xFFF25CAF)
+val PrimaryGrey = Color(0xFF3C4659)
+val PrimaryBlue = Color(0xFF04B2D9)
+val SecondaryGreen = Color(0xFF99BF60)
+val PrimaryOrange = Color(0xFFF2B05E)

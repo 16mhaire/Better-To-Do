adcaa9a13619abd61ab29baeecbd05f6fdb7be42
4realwth <107648207+4realwth@users.noreply.github.com>
2023-11-09 05:43:58 -0500

    please for the love of pete commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
 
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "BetterTo-Do"
-include(":app")
- 

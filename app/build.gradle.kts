ec9e3ea75eb5e9b6eed9b519107dd3123ea8d89f
MatthewTome <matthew.tome95@gmail.com>
2023-11-15 18:53:05 -0700

    Project builds cleanly! This is the most up-to-date version of our app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,4 +133,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
+
+    implementation ("androidx.work:work-runtime-ktx:2.8.1")
 }

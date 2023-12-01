93d3167967bcba60c239868d2b89a33d27a618dc
kevin stoelting <kevin.stoelting@gmail.com>
2023-11-30 16:28:52 -0800

    Adding my SavedListScreen and LabelManagement.kt

--- app/src/main/java/com/example/betterto_do/MainActivity.kt
+++ app/src/main/java/com/example/betterto_do/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background,
                 ) {
                     Dashboard()
+                    //ListCreationScreen()
                 }
             }
         }

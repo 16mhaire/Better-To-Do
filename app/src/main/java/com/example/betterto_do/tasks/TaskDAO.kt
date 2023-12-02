449ea16c2cd89aa42a9d202ea69c9eb2ca7a9300
MatthewTome <matthew.tome95@gmail.com>
2023-12-02 14:17:31 -0700

    Added Placeholder SubTask object within Task Class. Added SubTask functions.

--- app/src/main/java/com/example/betterto_do/tasks/TaskDAO.kt
+++ app/src/main/java/com/example/betterto_do/tasks/TaskDAO.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDAO {
+    // Task Functions
     @Query("select * from task_table order by id")
     fun getAllTask(): Flow<List<Task>>
 
@@ -31,4 +32,32 @@ interface TaskDAO {
 
     @Query("select * from task_table order by case when taskPriority like 'H%' then 1 when taskPriority like 'M%' then 2 when taskPriority like 'L%' then 3 end")
     fun sortByHighPriority(): Flow<List<Task>>
+
+    // SubTask Functions
+//    @Query("select * from task_table order by id")
+//    fun getAllSubTasks(): Flow<List<subTask>>
+
+//    @Query("select * from task_table where id = :id")
+//    fun getSelectedTask(id: Int): Flow<Task>
+
+    @Update
+    suspend fun updateSubTask(newSubTask: Task)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubTask(subTask: Task)
+
+    @Delete
+    suspend fun deleteSubTask(subTask: Task)
+
+//    @Query("delete from task_table")
+//    suspend fun deleteAllTask()
+
+//    @Query("select * from task_table where taskName like :searchQuery or taskDescription like :searchQuery")
+//    fun searchTasks(searchQuery: String): Flow<List<Task>>
+
+//    @Query("select * from task_table order by case when taskPriority like 'L%' then 1 when taskPriority like 'M%' then 2 when taskPriority like 'H%' then 3 end")
+//    fun sortByLowPriority(): Flow<List<Task>>
+
+//    @Query("select * from task_table order by case when taskPriority like 'H%' then 1 when taskPriority like 'M%' then 2 when taskPriority like 'L%' then 3 end")
+//    fun sortByHighPriority(): Flow<List<Task>>
 }

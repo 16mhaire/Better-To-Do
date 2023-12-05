b0644c48e86d7321e56648a9135aaf7039a3b4c6
Jaiten Beck <jaitenbeck@yahoo.com>
2023-12-04 17:56:18 -0700

    Running the App
    Didn't make any major changes, this builds though *Builds*

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/betterto_do/TaskScreenKt.java
@@ -0,0 +1,12 @@
+package com.example.betterto_do;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0018\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a&\u0010\u0000\u001a\u00020\u00012\u0012\u0010\u0002\u001a\u000e\u0012\u0004\u0012\u00020\u0004\u0012\u0004\u0012\u00020\u00010\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u0006H\u0007\u00a8\u0006\u0007"}, d2 = {"TaskScreen", "", "onNavigate", "Lkotlin/Function1;", "Lcom/example/betterto_do/util/UiEvent$Navigate;", "viewModel", "Lcom/example/betterto_do/viewmodels/SharedViewModel;", "app_debug"})
+public final class TaskScreenKt {
+    
+    @android.annotation.SuppressLint(value = {"UnusedMaterialScaffoldPaddingParameter"})
+    @androidx.compose.runtime.Composable
+    public static final void TaskScreen(@org.jetbrains.annotations.NotNull
+    kotlin.jvm.functions.Function1<? super com.example.betterto_do.util.UiEvent.Navigate, kotlin.Unit> onNavigate, @org.jetbrains.annotations.NotNull
+    com.example.betterto_do.viewmodels.SharedViewModel viewModel) {
+    }
+}

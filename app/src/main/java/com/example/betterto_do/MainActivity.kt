a6b275169037512f9504acd795b049eca12947f1
MatthewTome <matthew.tome95@gmail.com>
2023-12-07 19:37:09 -0700

    Added Fragments and ability to call .XML files to app. Most Up-To-Date version.

--- app/src/main/java/com/example/betterto_do/MainActivity.kt
+++ app/src/main/java/com/example/betterto_do/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.betterto_do.tasks.DashboardActivity
-import com.example.betterto_do.tasks.TaskFragment
 import com.example.betterto_do.ui.theme.BetterToDoTheme
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -25,23 +23,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TaskFragment()
-//                    val isLoggedIn = true
+                    val isLoggedIn = true
 //                    val isLoggedIn = FirebaseAuth.getInstance().currentUser != null
-//                    if (isLoggedIn) {
-//                        DashboardActivity()
+                    if (isLoggedIn) {
+                        setContentView(R.layout.fragment_add)
+//                        Dashboard()
 //                    } else {
-//                         Navigate to Login Activity
+//                        // Navigate to Login Activity
 //                        navigateToLogin()
 //                    }
+                    }
                 }
             }
         }
     }
+}
 
 //    private fun navigateToLogin() {
 //        val intent = Intent(this, Login::class.java)
 //        startActivity(intent)
 //        finish() // Finish MainActivity to prevent returning to it after logging in
 //    }
-}
+

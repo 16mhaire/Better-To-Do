9d7ef09d0ae88e9a227f984c38fe64f28015b1e9
MatthewTome <matthew.tome95@gmail.com>
2023-12-17 15:01:11 -0700

    Imported Login functionality

--- app/src/main/java/com/example/betterto_do/MainActivity.kt
+++ app/src/main/java/com/example/betterto_do/MainActivity.kt
@@ -6,24 +6,55 @@ import android.view.View
 import com.example.betterto_do.edittask.EditFragment
 import com.example.betterto_do.listtasks.ListFragment
 import java.util.*
+import android.content.Intent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.betterto_do.ui.theme.BetterToDoTheme
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity(), ListFragment.Callbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        setContent {
+            BetterToDoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    var isLoggedIn = FirebaseAuth.getInstance().currentUser
+                    if (true){
+                        setContentView(R.layout.activity_main)
 
-        setContentView(R.layout.activity_main)
-
-        // Check if the fragment container is empty and add ListFragment if it is
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-        if(currentFragment == null){
-            val fragment = ListFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
+                        // Check if the fragment container is empty and add ListFragment if it is
+                        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+                        if(currentFragment == null){
+                            val fragment = ListFragment()
+                            supportFragmentManager
+                                .beginTransaction()
+                                .add(R.id.fragment_container, fragment)
+                                .commit()
+                    }
+                    else{
+                        val intent = Intent(this, Login::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
         }
     }
+}
 
     // Callback method for task selection in ListFragment
     override fun onTaskSelect(view: View?, taskId: UUID) {
